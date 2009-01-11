6e1689e191abd0add9b5983f22906767bd52e4d5
mrenault <mrenault@d7cbbf1d-c55f-0410-8fef-9ee9f2c79e94>
2009-01-11 14:20:31 +0000

    initial commit

---
+++ trunk/Molecule.Console/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDesk.Options;
+using Molecule.Web.Security;
+
+
+namespace Molecule.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+
+            if (!Services.HasUsers)
+            {
+                createUser(null, null, true);
+                return;
+            }
+
+            bool showHelp = args.Length == 0;
+
+
+            var options = new OptionSet(){
+                {"h|?|help","show this message.",v => showHelp = true},
+                {"l|list-users", "list all users.", v => printUsers()},
+                {"au|add-user=", "add a new user.", (user,password) => createUser(user, password, false)},
+                {"aa|add-admin=", "add a new user with administration rights.", (user,password) => createUser(user, password, true)}
+            };
+
+            try
+            {
+                options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            
+
+            if (showHelp)
+                options.WriteOptionDescriptions(System.Console.Out);
+
+        }
+
+        private static void createUser(string username, string password, bool isAdmin)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                username = Environment.UserName;
+                System.Console.Write("Enter username (" + username + "): ");
+                string inputUsername = System.Console.ReadLine();
+                if (!String.IsNullOrEmpty(inputUsername))
+                    username = inputUsername;
+            }
+
+            while (String.IsNullOrEmpty(password))
+            {
+                System.Console.Write("Enter password for user " + username + ": ");
+                password = System.Console.ReadLine();
+                if (String.IsNullOrEmpty(password))
+                    System.Console.Write("Password can't be empty !");
+            }
+
+            Services.CreateUser(username, password, isAdmin);
+        }
+
+        private static void printUsers()
+        {
+            foreach (var user in Services.GetAllUsers())
+            {
+                System.Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ trunk/Molecule/Services.cs
@@ -0,0 +1,69 @@
+//
+// Service.cs
+//
+// Copyright (c) 2009 Pascal Fresnay (dev.molecule@free.fr) - Mickael Renault (dev.molecule@free.fr) 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+	
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Molecule.Web.Security;
+using System.Web.Security;
+
+namespace Molecule
+{
+    public static class Services
+    {
+
+        static SQLiteMembershipProvider mprovider = new SQLiteMembershipProvider();
+
+        static Services()
+        {
+
+        }
+
+        public static bool HasUsers
+        {
+            get
+            {
+                int nbUsers;
+                mprovider.GetAllUsers(0, 1, out nbUsers);
+                return nbUsers > 0;
+            }
+        }
+        public static void CreateUser(string login, string password, bool isAdmin)
+        {
+            MembershipCreateStatus status;
+            Membership.CreateUser(login, password, "", null, null, true, null, out status);
+            if (isAdmin)
+            {
+                Roles.AddUsersToRoles(new string[] { login },
+                    new string[] { SQLiteProvidersHelper.AdminRoleName });
+            }
+        }
+
+        public static IEnumerable<string> GetAllUsers()
+        {
+            int nbUsers;
+            foreach (var user in mprovider.GetAllUsers(0, Int32.MaxValue, out nbUsers))
+                yield return user.ToString();
+        }
+    }
+}
