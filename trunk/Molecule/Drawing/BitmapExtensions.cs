bdfb4e963b6a35a00a19af2b5c97eed440716703
pfresnay <pfresnay@d7cbbf1d-c55f-0410-8fef-9ee9f2c79e94>
2009-02-16 23:42:23 +0000

    add square resizing methods

--- trunk/Molecule/Drawing/BitmapExtensions.cs
+++ trunk/Molecule/Drawing/BitmapExtensions.cs
@@ -9,30 +9,54 @@ namespace Molecule.Drawing
 {
     public static class BitmapExtensions
     {
-        public static Bitmap GetResized(this Bitmap bitmap, int width, int height)
+        private static Bitmap resize(Bitmap bitmap, int width, int height, Rectangle? clipRectangle)
         {
             var src = Surface.CopyFromBitmap(bitmap);
             var dest = new Surface(width, height);
             dest.FitSurface(ResamplingAlgorithm.SuperSampling, src);
-            return dest.CreateAliasedBitmap();
+            if(clipRectangle == null)
+                return dest.CreateAliasedBitmap();
+            else
+                return dest.CreateAliasedBitmap(clipRectangle.Value);
         }
 
-        public static Bitmap GetResized(this Bitmap bitmap, int maxSize)
+        private static Bitmap resize(Bitmap bitmap, int maxSize, bool clipAsSquare)
         {
-            double ratio = bitmap.Width / bitmap.Height;
+            double ratio = (double)bitmap.Width / bitmap.Height;
             int destWidth;
             int destHeight;
-            if (bitmap.Width > bitmap.Height)
+            Rectangle? clipRectangle = null;
+            bool landscape = bitmap.Width > bitmap.Height;
+            if (landscape != clipAsSquare)
             {
                 destWidth = maxSize;
-                destHeight = destWidth / ratio;
+                destHeight = (int)(destWidth / ratio);
+                if (clipAsSquare)
+                    clipRectangle = new Rectangle(0, (destHeight - maxSize) / 2, maxSize, maxSize);
             }
             else
             {
                 destHeight = maxSize;
-                destWidth = ratio * destHeight;
+                destWidth = (int)(ratio * destHeight);
+                if (clipAsSquare)
+                    clipRectangle = new Rectangle((destWidth - maxSize) / 2, 0, maxSize, maxSize);
             }
-            return bitmap.GetResized(destWidth, destHeight);
+            return resize(bitmap, destWidth, destHeight, clipRectangle);
+        }
+
+        public static Bitmap GetResized(this Bitmap bitmap, int width, int height)
+        {
+            return resize(bitmap, width, height, null);
+        }
+
+        public static Bitmap GetResized(this Bitmap bitmap, int maxSize)
+        {
+            return resize(bitmap, maxSize, false);
+        }
+
+        public static Bitmap GetSquare(this Bitmap bitmap, int maxSize)
+        {
+            return resize(bitmap, maxSize, true);
         }
     }
 }
