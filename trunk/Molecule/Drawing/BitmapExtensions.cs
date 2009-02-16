1efca19afed700f922beed9a3297e1ac3844fd45
pfresnay <pfresnay@d7cbbf1d-c55f-0410-8fef-9ee9f2c79e94>
2009-02-16 22:43:02 +0000

    add Bitmap helper that wrap pdnlib for image resizing.

---
+++ trunk/Molecule/Drawing/BitmapExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using PaintDotNet;
+
+namespace Molecule.Drawing
+{
+    public static class BitmapExtensions
+    {
+        public static Bitmap GetResized(this Bitmap bitmap, int width, int height)
+        {
+            var src = Surface.CopyFromBitmap(bitmap);
+            var dest = new Surface(width, height);
+            dest.FitSurface(ResamplingAlgorithm.SuperSampling, src);
+            return dest.CreateAliasedBitmap();
+        }
+
+        public static Bitmap GetResized(this Bitmap bitmap, int maxSize)
+        {
+            double ratio = bitmap.Width / bitmap.Height;
+            int destWidth;
+            int destHeight;
+            if (bitmap.Width > bitmap.Height)
+            {
+                destWidth = maxSize;
+                destHeight = destWidth / ratio;
+            }
+            else
+            {
+                destHeight = maxSize;
+                destWidth = ratio * destHeight;
+            }
+            return bitmap.GetResized(destWidth, destHeight);
+        }
+    }
+}
