b82c1841a59cfb83ba6e5384683a2f62b2e17ed2
pfresnay <pfresnay@d7cbbf1d-c55f-0410-8fef-9ee9f2c79e94>
2009-03-08 20:18:33 +0000

    move css variables extension in molecule

--- trunk/Molecule.WebSite/CssHandler.ashx.cs
+++ trunk/Molecule.WebSite/CssHandler.ashx.cs
@@ -10,6 +10,7 @@ using System.Web.UI;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.IO;
+using Molecule.Web;
 
 namespace Molecule.WebSite
 {
@@ -20,34 +21,12 @@ namespace Molecule.WebSite
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     public class CssHandler : IHttpHandler
     {
-        static Regex variablesRegex = new Regex(@"@variables\s*\{(?<decl>[^\}]*)\}", RegexOptions.Compiled);
-        static Regex keyValueRegex = new Regex(@"\s*(?<key>[^:]+)\s*:\s*(?<value>[^;]+)\s*;", RegexOptions.Compiled);
-        static Regex variableRefRegex = new Regex(@"var\((?<var>[^\)]+)\)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/css";
             
-            //Replace CSS Variables by CSS 2.1
-            //http://disruptive-innovations.com/zoo/cssvariables/
-            //TODO : cache
-            //TODO : handle @import
-
-            //search for variables definition & remove it from css
-            var pairs = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
             string css = File.ReadAllText(context.Request.PhysicalPath);
-            css = variablesRegex.Replace(css, delegate(Match m){
-                foreach (Match mkv in keyValueRegex.Matches(m.Groups[1].Value))
-                    pairs[mkv.Groups["key"].Value] = mkv.Groups["value"].Value;
-                return "";
-            });
-
-            var queryString = context.Request.QueryString;
-            for (int i = 0; i < queryString.Count; i++)
-                pairs[queryString.GetKey(i)] = queryString.Get(i);
-
-            //search for variable reference & replace it by its definition.
-            css = variableRefRegex.Replace(css, m => pairs[m.Groups["var"].Value]);
+            css = CssVariablesExtender.ExpandVariables(css, context.Request.QueryString);
 
             context.Response.Write(css);
         }
---
+++ trunk/Molecule/Web/CssVariablesExtender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+using Mono.Rocks;
+
+namespace Molecule.Web
+{
+    /// <summary>
+    /// Allow use of CSS Variables extensions
+    /// http://disruptive-innovations.com/zoo/cssvariables/
+    /// </summary>
+    public class CssVariablesExtender
+    {
+        static Regex variablesRegex = new Regex(@"@variables\s*\{(?<decl>[^\}]*)\}", RegexOptions.Compiled);
+        static Regex keyValueRegex = new Regex(@"\s*(?<key>[^:]+)\s*:\s*(?<value>[^;]+)\s*;", RegexOptions.Compiled);
+        static Regex variableRefRegex = new Regex(@"var\((?<var>[^\)]+)\)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static string ExpandVariables(string variablesCss)
+        {
+            return ExpandVariables(variablesCss, (pairs) => { });
+        }
+
+        public static string ExpandVariables(string variablesCss, IDictionary<string, string> overrides)
+        {
+            return ExpandVariables(variablesCss, (pairs) =>
+                    overrides.ForEach(kvp => pairs[kvp.Key] = kvp.Value));
+        }
+
+        public static string ExpandVariables(string variablesCss, NameValueCollection overrides)
+        {
+            return ExpandVariables(variablesCss, delegate(IDictionary<string, string> pairs){
+                for (int i = 0; i < overrides.Count; i++)
+                    pairs[overrides.GetKey(i)] = overrides.Get(i);
+            });
+        }
+
+        protected static string ExpandVariables(string variablesCss, Action<IDictionary<string,string>> overrideAction)
+        {
+            //TODO : cache
+            //TODO : handle @import
+
+            //search for variables definition & remove it from css
+            var pairs = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            string css = variablesCss;
+            css = variablesRegex.Replace(css, delegate(Match m){
+                foreach (Match mkv in keyValueRegex.Matches(m.Groups[1].Value))
+                    pairs[mkv.Groups["key"].Value] = mkv.Groups["value"].Value;
+                return "";
+            });
+
+            overrideAction(pairs);
+
+            //search for variable reference & replace it by its definition.
+            css = variableRefRegex.Replace(css, m => pairs[m.Groups["var"].Value]);
+            return css;
+        }
+    }
+}
