5220659eb63612b55c0eae323abd56a14c2d8d1d
pfresnay <pfresnay@d7cbbf1d-c55f-0410-8fef-9ee9f2c79e94>
2009-03-16 14:52:56 +0000

    WebPhoto.Providers.FS added

---
+++ trunk/WebPhoto/WebPhoto.Providers.FS/PhotoLibraryProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Molecule.Runtime;
+using System.IO;
+
+[assembly:PluginContainer]
+
+namespace WebPhoto.Providers.FS
+{
+    [Plugin("File System")]
+    public class PhotoLibraryProvider : IPhotoLibraryProvider
+    {
+        static string baseDir = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+        [IsUsablePlugin]
+        public static bool IsUsable
+        {
+            get
+            {
+                return Directory.Exists(baseDir);
+            }
+        }
+
+        #region IPhotoLibraryProvider Members
+
+        public void Initialize()
+        {
+            
+        }
+
+        public IEnumerable<ITag> GetRootTags()
+        {
+            yield return new Tag(new DirectoryInfo(baseDir), null);
+        }
+
+        public IEnumerable<string> TagsRecentlyAdded
+        { get { return null; } }
+
+        #endregion
+    }
+}
